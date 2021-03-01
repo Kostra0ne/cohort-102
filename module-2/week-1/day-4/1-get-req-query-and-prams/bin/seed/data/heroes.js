b0f0354d8652acb433baff01ad910bb77027bb7b
KostraOne <guillaume.amangoua@gmail.com>
2021-03-01 10:01:35 +0100

    removded unused files

--- module-2/week-1/day-4/1-get-req-query-and-prams/bin/seed/data/heroes.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-require("./../../config/mongodb");
-
-const HeroeModel = require("./../../model/Heroe")
-const heroes = require("./heroes.json");
-
-console.log(heroes);
-
-HeroeModel.insertMany(heroes);
